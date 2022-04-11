ae0613b45b1afd5c2aee3207dc64227036d21708
Adrian; akhor005 <akhor005@ucr.edu>
2022-04-11 11:22:48 -0700

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swiftware Stress Analyzer
+//
+//  Created by Adrian on 4/11/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
