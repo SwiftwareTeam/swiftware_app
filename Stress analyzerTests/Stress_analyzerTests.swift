7469f3b2a75af4d943290cd2065bf03e106337c3
Shawn Long <slong024@ucr.edu>
2022-04-26 22:30:26 -0700

    added model files

--- Stress analyzerTests/Stress_analyzerTests.swift
+++ Stress analyzerTests/Stress_analyzerTests.swift
@@ -29,5 +29,91 @@ class Stress_analyzerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+//        func testNewGetResponse() async throws {
+//            let url = URL(string: "http://127.0.0.1:8080/getResponses/shawnlong636")!
+//            let request = URLRequest(url: url)
+//
+//            let (data, response) = try await URLSession.shared.data(for: request)
+//
+//            XCTAssertNotNil(response)
+//            XCTAssertNotNil(data)
+//
+//            let httpResponse = response as? HTTPURLResponse
+//            let surveyResponse: [SurveyResponse]?
+//
+//            let decoder = JSONDecoder()
+//
+//            XCTAssertEqual(httpResponse?.statusCode, 200)
+//
+//            surveyResponse = try decoder.decode([SurveyResponse].self, from: data)
+//
+//            print(surveyResponse?[0].responses)
+//
+//            XCTAssertNotNil(surveyResponse)
+//
+//        }
+    //
+//    func testGetSurvey() async throws {
+//        let url = URL(string: "http:/swiftware.tech/getSurveys")!
+//        let request = URLRequest(url: url)
+//
+//        let (data, response) = try await URLSession.shared.data(for: request)
+//
+//        XCTAssertNotNil(response)
+//        XCTAssertNotNil(data)
+//
+//        let httpResponse = response as? HTTPURLResponse
+//        let surveys: [Survey]?
+//
+//        let decoder = JSONDecoder()
+//
+//        XCTAssertEqual(httpResponse?.statusCode, 200)
+//
+//        surveys = try decoder.decode([Survey].self, from: data)
+//    }
+
+//    func testGetNulSurveyResponse() async throws {
+//        let url = URL(string: "http://127.0.0.1:8080/Test")! // Endpoint deleted, just sends single SurveyResponse with a nil
+//        let request = URLRequest(url: url)
+//
+//        let (data, response) = try await URLSession.shared.data(for: request)
+//
+//        XCTAssertNotNil(response)
+//        XCTAssertNotNil(data)
+//
+//        let httpResponse = response as? HTTPURLResponse
+//        let surveyResponse: SurveyResponse?
+//
+//        let decoder = JSONDecoder()
+//
+//        XCTAssertEqual(httpResponse?.statusCode, 200)
+//
+//        surveyResponse = try decoder.decode(SurveyResponse.self, from: data)
+//
+//        let value: Int? = surveyResponse?.responses[1] ?? nil
+//
+//        if value == nil {
+//            print("Value is nil")
+//        } else {
+//            print("Value is not nil")
+//        }
+//    }
+//    func testCreateResposnse() async throws {
+//        let url = URL(string: "http://127.0.0.1:8080/createResponse")!
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//
+//        let surveyResponse = SurveyResponse(uid: "shawnlong636", surveyID: 1, responseType: "new", responses: [1: 3, 2: 2, 3:4])
+//
+//        let encoder = JSONEncoder()
+//        let encoded = try encoder.encode(surveyResponse)
+//
+//        let (data, response) = try await URLSession.shared.upload(for: request, from: encoded)
+
+//        let responseStr = String(decoding: data, as: UTF8.self)
+//        print(responseStr)
+
+
+//    }
 
 }
