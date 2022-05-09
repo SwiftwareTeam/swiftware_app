0149f68fceaa90e8f77f801f386a9610e0c958b7
Shawn Long <slong024@ucr.edu>
2022-05-08 18:59:16 -0700

    Added PersonalityScores property for Anlytics View Model

--- Stress analyzer/ViewModels/AnalyticsViewModel.swift
+++ Stress analyzer/ViewModels/AnalyticsViewModel.swift
@@ -13,11 +13,13 @@ final class AnalyticsViewModel: ObservableObject {
     /// of surveyID, and values of [ChartData] which
     /// represent the list of ChartData objects for
     /// each question of the survey.
-    @Published var ResponseRatesBySurvey : [Int : [ChartData]]
+    @Published var ResponseRatesBySurvey: [Int : [ChartData]]
+    @Published var PersonalityScores: [PersonalityScore]
 
     init() {
         // New class properties should be initialized in here
-        self.ResponseRatesBySurvey = [Int : [ChartData]]()
+        self.ResponseRatesBySurvey = [Int: [ChartData]]()
+        self.PersonalityScores = [PersonalityScore]()
     }
 
     // TODO: Implement Function
@@ -27,7 +29,12 @@ final class AnalyticsViewModel: ObservableObject {
     ///  on the server. So if the returned ChartData items from
     ///  the server for surveyID: 1 is `chartDataList`, then it
     ///  can be saved using `ResposneRatesBySurvey[1] = chartDataList`
-    func getAvgResponseRate(surveyID : Int) async {
+    func getAvgResponseRate(surveyID: Int) async {
 
+    }
+
+    // TODO: Implement Function
+    func personalityScore(forUser: Int) async {
+        
     }
 }
